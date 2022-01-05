90517b27d134dfe9fd67f9fa415acbc6011fd098
Michael Jeffries <michael.jeffries@gmail.com>
2022-01-04 18:10:28 -0800

    name

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - fatds',
-    title: 'fatds',
+    titleTemplate: '%s',
+    title: 'Spicy Rain',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
